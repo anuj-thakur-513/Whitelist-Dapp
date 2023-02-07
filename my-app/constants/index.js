8d38df7baaff983bba33ebab87aaca2b44323768
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-02-08 00:33:54 +0530

    debugged the code

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -35,7 +35,7 @@ export const abi = [
   },
   {
     inputs: [],
-    name: "numAddresseseWhitelisted",
+    name: "numAddressesWhitelisted",
     outputs: [
       {
         internalType: "uint8",
